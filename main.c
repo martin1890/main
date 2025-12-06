a5a24d7bd0c3dbcda8405468cc2b67b59bd37e33
martin1890 <36193985+martin1890@users.noreply.github.com>
2025-12-06 09:43:15 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -3,17 +3,24 @@ void handle_interrupt(unsigned _irq)
 {
 }
 
-
 #define SCREEN_WIDTH  320
 #define SCREEN_HEIGHT 240
-#define TRANSPARENT_COLOR 254
+#define TRANSPARENT_COLOR 0   // 0 = transparent in this example
 
+// Global pointer to VGA memory
+volatile char* VGA = (volatile char*)0x08000000;
 
+// Simple 4x4 sprite
+const unsigned char smiley_sprite[4 * 4] = {
+    0,   255, 255, 0,
+    255, 0,   0,   255,
+    255, 255, 255, 255,
+    0,   255, 255, 0
+};
 
-
-
-
-void draw_sprite(volatile char* VGA, int x, int y, const unsigned char* sprite, int sprite_width, int sprite_height)
+void draw_sprite(volatile char* VGA, int x, int y,
+    const unsigned char* sprite,
+    int sprite_width, int sprite_height)
 {
     for (int row = 0; row < sprite_height; row++) {
         for (int col = 0; col < sprite_width; col++) {
@@ -36,22 +43,15 @@ void draw_sprite(volatile char* VGA, int x, int y, const unsigned char* sprite, 
     }
 }
 
-
-
-
 void draw() {
-    // Create a pointer called VGA that points to the drawing area
-    volatile char* VGA = (volatile char*)0x08000000;
-
-    // Clear the entire VGA buffer area by writing the value 0 (=black)
+    // Clear the entire VGA buffer area by writing 0 (=black)
     for (int i = 0; i < 320 * 480; i++) {
         VGA[i] = 0;
     }
 
     int p = 0;
-    // Draw five rows of white pixels (remember that screen is 320 pixels wide and the color 255 is white)
+    // Draw five rows of changing pixels
     for (int i = 0; i < 320 * 5; i++) {
-
         p++;
         if (p == 256) {
             p = 0;
@@ -62,20 +62,12 @@ void draw() {
 
 int main()
 {
-    const unsigned char smiley_sprite[4 * 4] = {
-    0, 255, 255, 0,
-    255, 0,   0, 255,
-    255, 255, 255, 255,
-    0,   255, 255, 0
-    };
-
-
     draw();
 
+    // Now VGA is a global pointer, so we can use it here
     draw_sprite(VGA, 50, 50, smiley_sprite, 4, 4);
 
     // Enter a forever loop
-    while (1)
-    {
+    while (1) {
     }
 }
